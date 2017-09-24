6bcae9753052e4053eb4661a8f58eff2e0b3ef14
Austin Parisi <austinparisi1991@gmail.com>
2017-09-23 19:23:26 -0500

    update

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         authDomain: "train-schedule-92d98.firebaseapp.com",
         databaseURL: "https://train-schedule-92d98.firebaseio.com",
         projectId: "train-schedule-92d98",
-        storageBucket: "",
+        storageBucket: "train-schedule-92d98.appspot.com",
         messagingSenderId: "570898962232"
     };
     firebase.initializeApp(config);
@@ -14,7 +14,8 @@ $(document).ready(function() {
 
 
     // add train button
-    $("addTrainButton").on("click", function() {
+    $("#addTrainButton").on("click", function(event) {
+        event.preventDefault();
 
         var trainName = $("#trainNameInput").val().trim();
         var destination = $("#destinationInput").val().trim();
@@ -34,14 +35,45 @@ $(document).ready(function() {
             name: trainName,
             destination: destination,
             trainTime: firstTime,
-            frequency: frequency,
+            frequency: frequency
         };
 
-        database.push(newTrain);
+        database.ref().push(newTrain);
+
+        // clear inputs 
+
+        $("#trainNameInput").val("");
+        $("#destinationInput").val("");
+        $("#timeInput").val("");
+        $("#frequencyInput").val("");
 
 
-
+        //     // post to html
+        $("#name").html(snapshot.val().name);
+        $("#destination").html(snapshot.val().destination);
+        $("#time").html(snapshot.val().trainTime);
+        $("#frequency").html(snapshot.val().frequency);
     });
+            // firebase snapshot stuff
+    database.ref().on("child_added", function(childSnapshot) {
 
+        console.log(childSnapshot.val());
 
+        var firebaseName = childSnapshot.val().name;
+        var firebaseDestination = childSnapshot.val().destination;
+        var firebaseTrainTime = childSnapshot.val().trainTime;
+        var firebaseFrequency = childSnapshot.val().frequency;
+
+        // full list of items to the well
+        $("#trainTable").append("<div class='well'><span id='name'> " + firebaseName +
+            " </span><span id='destination'> " + firebaseDestination +
+            " </span><span id='frequency'> " + firebaseFrequency +
+            " </span><span id='next'> " + firebaseNext + " </span><span id='minutesAway'> " + firebaseAway + " </span></div>");
+
+        // moment stuff
+
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        }
+    );
 });
